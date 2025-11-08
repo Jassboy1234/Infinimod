d34668aa82eb3b3b4ee1ca315008199c8358d3af
Jassboy1234 <135118640+Jassboy1234@users.noreply.github.com>
2025-11-08 18:41:42 +0300

    Add files via upload

---
+++ src/Home.jsx
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import Hyperspeed from './Hyperspeed/Hyperspeed';
+import './App.css';
+import Navbar from './Navbar.jsx';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar />
+        <p className='hero'>
+          Welcome to Infinimod v1.2!
+        </p>
+        <Hyperspeed />
+      </header>
+    </div>
+  );
+}
+
+export default Home;
