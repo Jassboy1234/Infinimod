77b431f1eb3a79cdc7b65a56793ef7f76a4785f0
Jassboy1234 <135118640+Jassboy1234@users.noreply.github.com>
2025-11-08 18:49:01 +0300

    Remove unused Features import from index.js
    Removed import statement for Features component.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Features from './Features';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
